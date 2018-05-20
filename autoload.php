445ad1dd4a38a9fada8a156a9826e578619b80cd
Bruno Marques <brunorizzardi80014@hotmail.com>
2018-05-20 03:47:31 -0300

    Simple autoload include file

---
+++ autoload.php
@@ -0,0 +1,16 @@
+<?php
+
+//Config
+require 'app/config/Database.php';
+
+//Controller
+require 'app/controller/Controller.php';
+require 'app/controller/UserController.php';
+
+//Model
+require  'app/model/User.php';
+require  'app/model/Group.php';
+
+//Service
+require  'app/service/UserService.php';
+require  'app/service/UserGroup.php';

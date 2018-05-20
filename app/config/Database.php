537182b92509347c775a45bb4c483ddd3f3b953c
Bruno Marques <brunorizzardi80014@hotmail.com>
2018-05-20 03:46:34 -0300

    Database connection and config

---
+++ app/config/Database.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace app\config;
+
+class Database
+{
+    private $host;
+    private $user;
+    private $pass;
+    private $db;
+    private $conn;
+
+    public function __construct() {
+
+        $this->host = 'localhost:3307';
+        $this->user = 'root';
+        $this->pass = 'root';
+        $this->db = 'cadastrocrud';
+
+        try {
+            $this->conn = new \PDO("mysql:host=" . $this->host . ";dbname=" . $this->db, $this->user, $this->pass);
+        } catch (PDOException $exc) {
+            echo $exc->getMessage();
+        }
+    }
+
+    public function query($sql) {
+        return $this->conn->query($sql);
+    }
+
+    public function prepare($sql) {
+        return $this->conn->prepare($sql);
+    }
+
+}

54461c684b8294f0071aad0a36d4a2f9b847bf03
Bruno Marques <brunorizzardi80014@hotmail.com>
2018-05-21 02:54:42 -0300

    Added view renderer with extract() to generate variables for view

--- app/controller/Controller.php
+++ app/controller/Controller.php
@@ -5,7 +5,28 @@ namespace app\controller;
 
 class Controller
 {
+    public $content;
+
     public function __construct() {
 
+    }
+
+    public function view($viewPath, Array $options = null) {
+        ob_start();
+        ob_implicit_flush(false);
+        if (isset($options)) {
+            foreach ($options as $key => $value) {
+                $$key = $value;
+                extract($$key, EXTR_OVERWRITE);
+            }
+        }
+
+        require('./app/view/' . $viewPath . '.php');
+        $this->content = ob_get_clean();
+        $this->template('template');
+    }
+
+    public function template($templatePath) {
+        include './app/view/core/' . $templatePath . '.php';
     }
 }

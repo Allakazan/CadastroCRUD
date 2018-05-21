fdf03f9d3158e675e96e6bdf7e1cffa5abfa6511
Bruno Marques <brunorizzardi80014@hotmail.com>
2018-05-21 03:04:59 -0300

    Some logic implemented to work like a route

--- index.php
+++ index.php
@@ -3,6 +3,37 @@
 include 'autoload.php';
 
 use app\controller\UserController;
-echo '<pre>';
-print_r(UserController::getInstance()->get(1));
-print_r(UserController::getInstance()->list());
+
+
+if (!$_GET) { exit('Precisa de parametros GET'); }
+
+$params = $_GET;
+$fields = $_POST;
+
+switch ($params['action']) {
+    case 'edit':
+        UserController::getInstance()->get($params['id']);
+        break;
+    case 'new':
+        UserController::getInstance()->new();
+        break;
+    case 'list':
+        if (array_key_exists('message', $params)) {
+            UserController::getInstance()->list($params['message']);
+        } else {
+            UserController::getInstance()->list();
+        }
+        break;
+    case 'insert':
+        UserController::getInstance()->insert($fields);
+        break;
+    case 'update':
+        UserController::getInstance()->update($fields);
+        break;
+    case 'remove':
+        UserController::getInstance()->delete($params['id']);
+        break;
+    default:
+        echo 'Selecione uma opçao valida';
+        break;
+}
